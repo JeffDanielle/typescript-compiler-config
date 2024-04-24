96dc54a4295213e8c2a8013bf2b60c66e0647d46
JeffDanielle <bayotjeff@gmail.com>
2024-04-24 15:05:36 +0800

    38. Understanding TypeScript Core Libs

--- app.ts
+++ app.ts
@@ -1,6 +1,12 @@
 // tsc --init, tsc --watch
 // Automatically recompile any changes made in a typescript file
 
-const userName = "Jeff Pikoy";
+// const userName = "Jeff";
 
-console.log(userName);
+// console.log(userName);
+
+const buttonEl = document.querySelector("btn")!;
+
+buttonEl.addEventListener("click", () => {
+    console.log("clicked");
+});
